658115200c4c02cf60b4c2a500e25a2fe630d140
Ale De Alvino <alejandra.dealvino.melgoza@gmail.com>
2022-05-17 15:07:55 -0500

    cambio en registro

--- src/controllers/users_control.js
+++ src/controllers/users_control.js
@@ -15,31 +15,34 @@ export const register = async (req, res) => {
     
 
     if(Nombres && Apellidos && CURP && Fecha_nac && Nacionalidad && Sexo && Escolaridad && Ocupacion && Estado_civil && Hijos && Telefono && Email && Localidad && Municipio && Estado && CP && Pais && Foto_perfil && Username && Password && Fecha_reg && Pregunta_sec && Respuesta_sec){
-        var BCRYPT_SALT_ROUNDS =12   //variable para indicar los saltos a bcrypt
-        bcrypt.hash(Password, BCRYPT_SALT_ROUNDS)
-        .then(function(hashedPassword){
-            var password = hashedPassword;
-            console.log(password);
-            let query = "INSERT INTO socios (Nombres, Apellidos, CURP, Fecha_nac, Nacionalidad, Sexo, Escolaridad, Ocupacion, Estado_civil, Hijos, Telefono, Email, Localidad, Municipio, Estado, CP, Pais, Foto_perfil, Username, Password, Fecha_reg, Pregunta_sec, Respuesta_sec)";
-            query += `VALUES ('${Nombres}', '${Apellidos}', '${CURP}', '${Fecha_nac}', '${Nacionalidad}', '${Sexo}', '${Escolaridad}', '${Ocupacion}', '${Estado_civil}', '${Hijos}', '${Telefono}', '${Email}', '${Localidad}', '${Municipio}', '${Estado}', '${CP}', '${Pais}', '${Foto_perfil}', '${Username}', '${password}', '${Fecha_nac}', '${Pregunta_sec}', '${Respuesta_sec}')`;
-            db.query(query);
 
-            // if(rows.affectedRows == 1){
-            //     return res.status(201).json({code: 201, message: "usuario registrado correctamente" });
-            // }
-            // return res.status(500).json({code: 500, message: "Ocurrio un error" });
-            res.json({code: 200, message: 'Usuario guardado'});
-        })
-        .catch(function(error){
-            console.log("Error saving user: ");
-            console.log(error);
-            res.json({code: 500, message:'Algo salio mal'});
-        })
+        const rows = await db.query('SELECT * FROM socios WHERE Username = '+Username+';')
+        console.log(rows.length);
+
+        if(rows==0){
+            var BCRYPT_SALT_ROUNDS =12   //variable para indicar los saltos a bcrypt
+            bcrypt.hash(Password, BCRYPT_SALT_ROUNDS)
+            .then(function(hashedPassword){
+                var password = hashedPassword;
+                console.log(password);
+                let query = "INSERT INTO socios (Nombres, Apellidos, CURP, Fecha_nac, Nacionalidad, Sexo, Escolaridad, Ocupacion, Estado_civil, Hijos, Telefono, Email, Localidad, Municipio, Estado, CP, Pais, Foto_perfil, Username, Password, Fecha_reg, Pregunta_sec, Respuesta_sec)";
+                query += `VALUES ('${Nombres}', '${Apellidos}', '${CURP}', '${Fecha_nac}', '${Nacionalidad}', '${Sexo}', '${Escolaridad}', '${Ocupacion}', '${Estado_civil}', '${Hijos}', '${Telefono}', '${Email}', '${Localidad}', '${Municipio}', '${Estado}', '${CP}', '${Pais}', '${Foto_perfil}', '${Username}', '${password}', '${Fecha_nac}', '${Pregunta_sec}', '${Respuesta_sec}')`;
+                db.query(query);
+                res.json({code: 200, message: 'Usuario guardado'});
+            })
+            .catch(function(error){
+                console.log("Error saving user: ");
+                console.log(error);
+                res.json({code: 500, message:'Algo salio mal'});
+            })
+        }else{
+            res.json({code: 500, message:'Ya existe este username'});
+        }
         
     }else{
-        res.json({saludo: 'Hello es una prueba'});
+        return res.status(500).json({code: 500, message: "Campos incompletos" });
     }
-    // return res.status(500).json({code: 500, message: "Campos incompletos" });
+    
     
 }
 
