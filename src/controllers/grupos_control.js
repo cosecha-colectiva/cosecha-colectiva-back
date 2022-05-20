962841a56619aa57284114efc91107745b288ac4
JaviZamudio <javier.elihu100@gmail.com>
2022-05-19 19:11:29 -0500

    generar codigo de grupo por separado

--- src/controllers/grupos_control.js
+++ src/controllers/grupos_control.js
@@ -1,6 +1,5 @@
 const db = require('../../config/database');
-const random = require('string-random');
-import {Fecha_actual} from '../funciones_js/validaciones';
+import {Fecha_actual, generarCodigoValido} from '../funciones_js/validaciones';
 
 // Funcion para creacion de grupos
 export const crear_grupos = async (req, res) => {
@@ -8,25 +7,13 @@ export const crear_grupos = async (req, res) => {
     const {Nombre_grupo, Localidad, Municipio, Estado, CP, Pais} = req.body;
     // Verificar que los campos esten completos
     if(Nombre_grupo && Localidad && Municipio && Estado && CP && Pais){
-        var bandera = true;
-        while(bandera){
-            const rand = random(6, {letters: false});
-            //comprobar que el codigo de grupo no exista
-            let query = "SELECT * FROM grupos WHERE Codigo_grupo = ?";
-            var rows = await db.query(query, [rand]);
-            if(rows.length == 0){
-                bandera = false;
-                const Fecha_reg = Fecha_actual();
-                let query = "INSERT INTO grupos (Nombre_grupo, Codigo_grupo, Localidad, Municipio, Estado, CP, Pais, Fecha_reg)";
-                query += `VALUES ('${Nombre_grupo}', '${rand}', '${Localidad}', '${Municipio}', '${Estado}', '${CP}', '${Pais}', '${Fecha_reg}');`;
-                db.query(query);
-                return res.status(201).json({code: 201, message: "Grupo registrado correctamente" });
-            }
-            else{
-                bandera = true;
-            }
-            // bandera = false;
-        }
+        // Crear el codigo de grupo
+        const codigo_grupo = await generarCodigoValido();
+        const Fecha_reg = Fecha_actual();
+        let query = "INSERT INTO grupos (Nombre_grupo, Codigo_grupo, Localidad, Municipio, Estado, CP, Pais, Fecha_reg) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        const rows = await db.query(query, [Nombre_grupo, codigo_grupo, Localidad, Municipio, Estado, CP, Pais, Fecha_reg]);
+        return res.status(201).json({code: 201, message: "Grupo registrado correctamente" });
+
     }else{
         //campos incompletos
         res.status(400).json({code: 400, message: 'Campos incompletos'});
