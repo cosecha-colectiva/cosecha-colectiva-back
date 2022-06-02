b8286468595df8aca0dea9f0df7c92d4810cac36
JaviZamudio <javier.elihu100@gmail.com>
2022-06-01 23:03:03 -0500

    pase de asistencia individual y de grupo

--- src/controllers/sesiones_control.js
+++ src/controllers/sesiones_control.js
@@ -7,6 +7,13 @@ Caja -- got from caja of previous session by a query to db
 Grupo_id
 */
 
+/* Estructura de la tabla Asistencias
+Asistencia_id
+Presente
+Sesion_id
+Socio_id
+*/
+
 //crear nueva sesion
 export const crear_sesion = async (req, res) => {
     const Fecha_sesion = Fecha_actual();
@@ -76,6 +83,85 @@ export const actualizar_caja = async (req, res) => {
     {
         "Caja": 100,
         "Sesion_id": 4 
+    }
+    */
+}
+
+// registrar asistencia individual
+export const registrar_asistencia = async (req, res) => {
+    const {Presente, Sesion_id, Socio_id} = req.body;
+
+    //comprobar que haya Presente, Sesion_id y Socio_id
+    if(Presente && Sesion_id && Socio_id){
+        //registrar asistencia
+        try{
+            let query = "INSERT INTO asistencias (Presente, Sesion_id, Socio_id) VALUES (?, ?, ?)";
+            await db.query(query, [Presente, Sesion_id, Socio_id]);
+            res.json({code: 200, message: 'Asistencia registrada'}).status(200);
+        }catch(err){
+            res.json({code: 500, message: 'Error al registrar asistencia'}).status(500);
+        }
+    }else{
+        // campos incompletos
+        res.json({code: 400, message: 'Campos incompletos'}).status(400);
+    }
+
+    /* Codigos de respuesta
+    200: Asistencia registrada
+    400: Campos incompletos
+    500: Error al registrar asistencia
+    */
+
+    /* Json de prueba
+    {
+        "Presente": 1,
+        "Sesion_id": 4,
+        "Socio_id": 54
+    }
+    */
+}
+
+// registrar asistencias de un grupo
+//recibe el id de la sesion y un array de json con {Socio_id, Presente}
+export const registrar_asistencias = async (req, res) => {
+    const {session_id, socios} = req.body;
+
+    //comprobar que haya session_id y socios
+    if(session_id && socios){
+        //registrar asistencias
+        try{
+            for(let i = 0; i < socios.length; i++){
+                let query = "INSERT INTO asistencias (Presente, Sesion_id, Socio_id) VALUES (?, ?, ?)";
+                await db.query(query, [socios[i].Presente, session_id, socios[i].Socio_id]);
+            }
+            res.json({code: 200, message: 'Asistencias registradas'}).status(200);
+        }catch(err){
+            res.json({code: 500, message: 'Error al registrar asistencias'}).status(500);
+        }
+    }else{
+        // campos incompletos
+        res.json({code: 400, message: 'Campos incompletos'}).status(400);
+    }
+
+    /* Codigos de respuesta
+    200: Asistencias registradas
+    400: Campos incompletos
+    500: Error al registrar asistencias
+    */
+
+    /* Json de prueba
+    {
+        "session_id": 4,
+        "socios": [
+            {
+                "Socio_id": 54,
+                "Presente": 1
+            },
+            {
+                "Socio_id": 64,
+                "Presente": 0
+            }
+        ]
     }
     */
 }
--- src/routes/sesiones_routes.js
+++ src/routes/sesiones_routes.js
@@ -1,8 +1,10 @@
-const { crear_sesion, actualizar_caja } = require('../controllers/sesiones_control');
+const { crear_sesion, actualizar_caja, registrar_asistencias, registrar_asistencia } = require('../controllers/sesiones_control');
 
 const router = require('express').Router();
 
 router.post("/crear_sesion", crear_sesion);
 router.post("/actualizar_caja", actualizar_caja);
+router.post("/registrar_asistencia", registrar_asistencia);
+router.post("/registrar_asistencias", registrar_asistencias);
 
 module.exports = router;
