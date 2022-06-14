e43bcff035a5fa98fa816765fcf7f63a16de52d5
JaviZamudio <javier.elihu100@gmail.com>
2022-06-14 18:47:38 -0500

    funciones de multas sin awaits y mejoradas

--- src/controllers/multas_control.js
+++ src/controllers/multas_control.js
@@ -10,99 +10,112 @@ const multas_control = {
             return res.json({ code: 400, message: 'Campos incompletos' }).status(400);
         }
 
-        const query = "SELECT * FROM multas WHERE Grupo_id = ?";
-
-        db.query(query, [grupo_id])
-            .then(rows => {
-                res.json({ code: 200, message: 'Multas obtenidas', data: rows }).status(200);
-            })
-            .catch(err => {
-                res.json({ code: 500, message: 'Error al obtener multas' }).status(500);
-            });
+        try {
+            const query = "SELECT * FROM multas WHERE Grupo_id = ?";
+            const multas = db.query(query, [grupo_id]);
+            return res.json({ code: 200, message: 'Multas obtenidas', data: multas }).status(200);
+        } catch (err) {
+            console.log(err);
+            return res.json({ code: 500, message: 'Error al obtener multas' }).status(500);
+        }
     },
 
     // POST
     crear_multa: (req, res) => {
-        const campos_multas = [
-            "Monto_multa",
-            "Descripcion",
-            "Sesion_id",
-            "Socio_id"
-        ];
+        const campos_multa = {
+            Monto_multa: req.body.Monto_multa,
+            Descripcion: req.body.Descripcion,
+            Sesion_id: req.body.Sesion_id,
+            Socio_id: req.body.Socio_id
+        };
 
-        const valores_campos = [];
-
-        campos_multas.forEach(campo => {
-            if (req.body[campo]) {
-                valores_campos.push(req.body[campo]);
+        for (const key in campos_multa) {
+            if (!campos_multa[key]) {
+                return res.json({ code: 400, message: 'Campos incompletos' }).status(400);
             }
-        });
+        }
 
-        if (valores_campos.length !== campos_multas.length) {
+        try {
+            const query = "INSERT INTO multas SET ?";
+            const result = db.query(query, campos_multa);
+            return res.json({ code: 200, message: 'Multa creada' }).status(200);
+        } catch (err) {
+            console.log(err);
+            return res.json({ code: 500, message: 'Error al crear multa' }).status(500);
+        }
+    },
+
+    pagar_multas: (req, res) => {
+        const { Multas } = req.body;
+
+        if (!Multas) {
             return res.json({ code: 400, message: 'Campos incompletos' }).status(400);
         }
 
-        const query = "INSERT INTO multas (" + campos_multas.join(", ") + ") VALUES (" + campos_multas.map(() => "?").join(", ") + ")";
+        try {
+            const error = false;
+            for (const multa of Multas) {
+                const resultado = pagar_multa(multa.Multa_id);
+                if (resultado === -1) {
+                    error = true;
+                }
+            }
 
-        db.query(query, valores_campos)
-            .then(result => {
-                return res.status(200).json({ code: 200, message: 'Multa creada' });
-            })
-            .catch(err => {
-                return res.status(500).json({ code: 500, message: 'Error al crear multa' });
-            });
-    },
-
-    pagar_multa: (req, res) => {
-        const { Multa_id } = req.body;
-
-        if (!Multa_id) {
-            return res.status(400).json({ code: 400, message: 'Campos incompletos' });
+            if (error) {
+                return res.json({ code: 500, message: 'Error al pagar alguna multa' }).status(500);
+            }
+            return res.json({ code: 200, message: 'Multas pagadas' }).status(200);
+        } catch (err) {
+            console.log(err);
+            return res.json({ code: 500, message: 'Error al pagar multas' }).status(500);
         }
-
-        // obtener datos de multa
-        let query = "SELECT * FROM multas WHERE Multa_id = ?";
-        db.query(query, [Multa_id])
-            .then(result => {
-                // validar que la multa exista
-                if (result.length === 0) {
-                    return res.status(404).json({ code: 404, message: 'Multa no encontrada' });
-                }
-
-                // extraer datos de multa
-                const { Socio_id, Monto_multa, Sesion_id } = result[0];
-
-                // obtener datos de la sesion
-                query = "SELECT * FROM sesiones WHERE Sesion_id = ?";
-                const sesiones = await db.query(query, [Sesion_id]);
-                const { Caja_cierre } = sesiones[0];
-
-                // establecer campos de la multa
-                const campos_multas = {
-                    Cantidad_movimiento: -Monto_multa,
-                    Caja: Caja_cierre,
-                    Socio_id,
-                    Sesion_id,
-                    Cat_id: "PAGO_MULTA"
-                }
-
-                // crear Transaccion
-                query = "INSERT INTO transacciones SET ?";
-                const transaccion = await db.query(query, campos_multas);
-
-                // Actualizar Status y Transaccion_id de multa
-                query = "UPDATE multas SET Status = 1, Transaccion_id = ? WHERE Multa_id = ?";
-                await db.query(query, [transaccion.insertId, Multa_id]);
-
-                // Actualizar Caja_cierre de sesion
-                actualizar_caja(Sesion_id, Monto_multa);
-
-                return res.status(200).json({ code: 200, message: 'Multa pagada' });
-            })
-            .catch(err => {
-                return res.status(500).json({ code: 500, message: 'Error al pagar multa' });
-            });
     },
 }
 
 module.exports = multas_control;
+
+function pagar_multa (Multa_id) {
+    // obtener datos de multa
+    let query = "SELECT * FROM multas WHERE Multa_id = ?";
+    const multas = db.query(query, [Multa_id])
+
+    // verificar que la multa no este pagada
+    if (multas[0].Status != 0) {
+        return -1;
+    }
+
+    // validar que la multa exista
+    if (multas.length === 0) {
+        return -1;
+    }
+
+    // extraer datos de multa
+    const { Socio_id, Monto_multa, Sesion_id } = multas[0];
+    
+    // obtener datos de la sesion
+    query = "SELECT * FROM sesiones WHERE Sesion_id = ?";
+    const sesion = db.query(query, [Sesion_id]);
+    const { Caja_cierre } = sesion[0];
+
+    // establecer campos de la multa
+    const campos_transaccion = {
+        Cantidad_movimiento: -Monto_multa,
+        Caja: Caja_cierre,
+        Socio_id: Socio_id,
+        Sesion_id: Sesion_id,
+        Cat_id: "PAGO_MULTA"
+    }
+
+    // crear Transaccion
+    query = "INSERT INTO transacciones SET ?";
+    const resultado = db.query(query, campos_transaccion);
+    
+    // Actualizar Status y Transaccion_id de multa
+    query = "UPDATE multas SET Status = 1, Transaccion_id = ? WHERE Multa_id = ?";
+    db.query(query, [resultado.insertId, Multa_id]);
+
+    // Actualizar Caja_cierre de sesion
+    if(!actualizar_caja(Sesion_id, -Monto_multa)){
+        return -1;
+    }
+}
--- src/funciones_js/Sesiones.js
+++ src/funciones_js/Sesiones.js
@@ -1,16 +1,16 @@
 const db = require("../../config/database")
 
-const actualizar_caja = async (sesion_id, monto) => {
+const actualizar_caja = (sesion_id, monto) => {
     // sumar o restar monto de Caja_cierre
     const query = "UPDATE sesiones SET Caja_cierre = Caja_cierre + ? WHERE Sesion_id = ?";
 
-    db.query(query, [monto, sesion_id])
-        .then(result => {
-            return { code: 200, message: 'Caja actualizada' };
-        })
-        .catch(err => {
-            return { code: 500, message: 'Error al actualizar caja' };
-        });
+    try{
+        db.query(query, [monto, sesion_id]);
+        return true;
+    }
+    catch(err){
+        return false;
+    }
 }
 
 module.exports = { 
