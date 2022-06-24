085bdff90848e2b1cc330907a90c136210ac1b89
Ale De Alvino <alejandra.dealvino.melgoza@gmail.com>
2022-06-24 11:02:36 -0500

    Funcion agregar usuarios a grupos

--- src/controllers/grupos_control.js
+++ src/controllers/grupos_control.js
@@ -28,14 +28,37 @@ export const crear_grupo = async (req, res) => {
     }
 }
 
-export const agregar_socio = (req, res) => {
+export const agregar_socio = async (req, res) => {
     const {Socio_id, Codigo_grupo} = req.body;
+    console.log(Socio_id, Codigo_grupo);
 
-    // Obtener el id del grupo con ese codigo
-    let query = "Select Grupo_id from grupos where Codigo_grupo = ?";
-    
-    db.query(query, [Codigo_grupo])
-        .then(({Grupo_id}) => {
-            query = "Insert into grupo_socio ()"
-        })
+    if(Socio_id && Codigo_grupo){
+        // Verificar que existe el grupo y obtener el id del grupo con ese codigo
+        let query = "SELECT Grupo_id FROM grupos WHERE Codigo_grupo = ?";
+        const grupo_id = await db.query(query, [Codigo_grupo]);
+        if(grupo_id.length == 0){
+            return res.status(500).json({code: 500, message: 'Este grupo no existe'});
+        }
+        // Verificar que existe el socio y obtener el id del grupo con ese codigo
+        let query2 = "SELECT * FROM socios WHERE Socio_id = ?";
+        const socio = await db.query(query2, [Socio_id]);
+        if(socio.length == 0){
+            return res.status(500).json({code: 500, message: 'Este socio no existe'});
+        }
+        // Verificar que el socio no este ya agregado a este grupo y este activo
+        let query3 = "SELECT * FROM grupo_socio WHERE Socio_id = ? AND Grupo_id = ? AND Status = 1";
+        const union = await  db.query(query3, [Socio_id, Codigo_grupo]);
+        if(union.length == 0){
+            try{
+                let query4 = "INSERT INTO grupo_socio (Grupo_id, Socio_id) VALUES (?, ?) ";
+                db.query(query4, [grupo_id[0].Grupo_id, Socio_id]);
+                return res.status(201).json({code: 201, message: "Socio agregado al grupo correctamente" });
+            }catch{
+                return res.status(500).json({code: 500, message: 'Error al agregar un socio al grupo'});
+            }
+        }else{
+            return res.status(500).json({code: 500, message: 'Este usuario ya esta agregado al grupo'});
+        }
+    }
+    return res.status(500).json({code: 500, message: 'Campos incompletos'});
 }
--- src/routes/grupos_routes.js
+++ src/routes/grupos_routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import {crear_grupo } from "../controllers/grupos_control";
+import {crear_grupo, agregar_socio } from "../controllers/grupos_control";
 
 const router = Router()
 
 router.post('/crear_grupo', crear_grupo);
+router.post('/agregar_socio', agregar_socio);
 
 export default router
