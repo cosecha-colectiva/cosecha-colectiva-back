0ef3702a14ab5d63bd8d1178cdbe149facc2467e
Ale De Alvino <alejandra.dealvino.melgoza@gmail.com>
2022-06-30 17:45:58 -0500

    agregar una pregunta al catalogo de preguntas de seguridad

--- src/routes/transacciones_admin_routes.js
+++ src/routes/transacciones_admin_routes.js
@@ -1,7 +1,8 @@
-const { agregar_catalogo_transaccion } = require('../controllers/transacciones_admin_control');
+const { agregar_catalogo_transaccion, agregar_catalogo_preguntas_seguridad } = require('../controllers/transacciones_admin_control');
 
 const router = require('express').Router();
 
 router.post("/agregar_catalogo_transaccion", agregar_catalogo_transaccion);
+router.post("/agregar_catalogo_preguntas_seguridad", agregar_catalogo_preguntas_seguridad);
 
 module.exports = router;
