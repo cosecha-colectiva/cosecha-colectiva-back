1b4e76cb8696b4f84db0c2b9c8018c8deea4f674
JaviZamudio <javier.elihu100@gmail.com>
2022-07-31 10:50:56 -0500

    Patron para usar un reqBody en los tests

--- tests/functional/socios.test.ts
+++ tests/functional/socios.test.ts
@@ -8,46 +8,52 @@ import { random, formatearFecha } from "../../src/utils/utils";
 const request = supertest(app);
 
 describe("Login", () => {
+    const reqBody = { Username: config.Javi.username, Password: config.Javi.password }
 
     it("Debería devolver un token si las credenciales son correctas", async () => {
-        const response = await request.post("/login").send({ Username: config.Javi.username, Password: config.Javi.password });
+        const response = await request.post("/login").
+            send(reqBody);
 
         expect(response.body).toHaveProperty("token");
     })
 
     it("Debería devolver un codigo 401 si la contraseña es incorrecta", async () => {
-        const response = await request.post("/login").send({ Username: config.Javi.username, Password: config.Javi.password + "hola" });
+        const response = await request.post("/login")
+            .send({ ...reqBody, Password: config.Javi.password + "hola" });
+            
         expect(response.statusCode).toBe(401);
     })
 })
 
 describe("Register", () => {
+    const reqBody = {
+        Nombres: falso.randFirstName(),
+        Apellidos: falso.randLastName(),
+        CURP: falso.randUuid(),
+        Fecha_nac: formatearFecha(falso.randPastDate({ years: 70 })),
+        Nacionalidad: falso.randCountry(),
+        Sexo: random("H", "M"),
+        Escolaridad: random("Posgrado", "Licenciatura", "Preparatoria", "Secundaria", "Primaria"),
+        Ocupacion: falso.randJobTitle(),
+        Estado_civil: random("Soltero", "casado"),
+        Hijos: falso.randNumber({ min: 0, max: 3 }),
+        Telefono: falso.randPhoneNumber(),
+        Email: falso.randEmail(),
+        Localidad: falso.randCounty(),
+        Municipio: falso.randCity(),
+        Estado: falso.randState(),
+        CP: falso.randZipCode(),
+        Pais: falso.randCounty(),
+        Foto_perfil: falso.randAvatar(),
+        Username: falso.randUserName(),
+        Password: falso.randPassword(),
+        Pregunta_id: random(4, 14),
+        Respuesta: falso.randFirstName()
+    }
+
     it("Debería devolver un status de 201", async () => {
         const response = await request.post("/register")
-            .send({
-                Nombres: falso.randFirstName(),
-                Apellidos: falso.randLastName(),
-                CURP: falso.randUuid(),
-                Fecha_nac: formatearFecha(falso.randPastDate({ years: 70 })),
-                Nacionalidad: falso.randCountry(),
-                Sexo: random("H", "M"),
-                Escolaridad: random("Posgrado", "Licenciatura", "Preparatoria", "Secundaria", "Primaria"),
-                Ocupacion: falso.randJobTitle(),
-                Estado_civil: random("Soltero", "casado"),
-                Hijos: falso.randNumber({ min: 0, max: 3 }),
-                Telefono: falso.randPhoneNumber(),
-                Email: falso.randEmail(),
-                Localidad: falso.randCounty(),
-                Municipio: falso.randCity(),
-                Estado: falso.randState(),
-                CP: falso.randZipCode(),
-                Pais: falso.randCounty(),
-                Foto_perfil: falso.randAvatar(),
-                Username: falso.randUserName(),
-                Password: falso.randPassword(),
-                Pregunta_id: random(4, 14),
-                Respuesta: falso.randFirstName()
-            });
+            .send(reqBody);
 
         expect(response.statusCode).toBe(201);
     });
