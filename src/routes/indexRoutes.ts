ae79db694d713e8f005ed1790909003b9f5eaa44
JaviZamudio <75400668+JaviZamudio@users.noreply.github.com>
2022-08-13 05:15:42 -0500

    test route

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { route } from "./admin_routes";
 import { gruposRoutes } from "./grupos/gruposRoutes";
 import { sociosRoutes } from "./socios/sociosRoutes";
 
@@ -6,5 +7,9 @@ const router = Router();
 
 router.use("/socios", sociosRoutes);
 router.use("/grupos", gruposRoutes);
+
+router.all("/", (req, res) => {
+    res.send(process.env)
+});
 
 export { router as indexRoutes };
