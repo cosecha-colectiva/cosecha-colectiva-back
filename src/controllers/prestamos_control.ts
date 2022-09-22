c264de857b4fa458375988c5f1cce50b72369466
Ale De Alvino <alejandra.dealvino.melgoza@gmail.com>
2022-09-22 17:17:22 -0500

    error con sesiones corregido

--- src/controllers/prestamos_control.ts
+++ src/controllers/prestamos_control.ts
@@ -36,43 +36,43 @@ export const crear_prestamo = async (req: AdminRequest<PayloadCrearPrestamos>, r
     const Grupo_id = Number(req.params.Grupo_id);
     const Socio_id = Number(req.params.Socio_id);
 
-    const acuerdoActual = await obtenerAcuerdoActual(Grupo_id);
-    const sesionActual = await obtenerSesionActual(Grupo_id);
-
     if (campos_incompletos({ Monto_prestamo, Num_sesiones, Observaciones, Estatus_ampliacion, Prestamo_original_id, Grupo_id, Socio_id })) {
         return res.status(400).json({ code: 400, message: 'Campos incompletos' });
-    }
-    
-
-    const campos_prestamo: Prestamo = {
-        Monto_prestamo,
-        Num_sesiones,
-        Observaciones,
-        Acuerdos_id: acuerdoActual.Acuerdo_id!,
-        Estatus_ampliacion,
-        Prestamo_original_id,
-        Estatus_prestamo: 0,
-        Fecha_final: formatearFecha(new Date(Date.now() + (Num_sesiones * acuerdoActual.Periodo_reuniones * 7 * 24 * 60 * 60 * 1000))),
-        Fecha_inicial: Fecha_actual(),
-        Interes_generado: 0,
-        Interes_pagado: 0,
-        Monto_pagado: 0,
-        Sesion_id: sesionActual.Sesion_id!,
-        Sesiones_restantes: Num_sesiones,
-        Socio_id: Socio_id,
     }
 
     const con = await db.getConnection();
     try {
+
+        const acuerdoActual = await obtenerAcuerdoActual(Grupo_id);
+        const sesionActual = await obtenerSesionActual(Grupo_id);
+
+        const campos_prestamo: Prestamo = {
+            Monto_prestamo,
+            Num_sesiones,
+            Observaciones,
+            Acuerdos_id: acuerdoActual.Acuerdo_id!,
+            Estatus_ampliacion,
+            Prestamo_original_id,
+            Estatus_prestamo: 0,
+            Fecha_final: formatearFecha(new Date(Date.now() + (Num_sesiones * acuerdoActual.Periodo_reuniones * 7 * 24 * 60 * 60 * 1000))),
+            Fecha_inicial: Fecha_actual(),
+            Interes_generado: 0,
+            Interes_pagado: 0,
+            Monto_pagado: 0,
+            Sesion_id: sesionActual.Sesion_id!,
+            Sesiones_restantes: Num_sesiones,
+            Socio_id: Socio_id,
+        }
+
         await con.beginTransaction();
 
         // Verificar que el socio pueda generar un prestamo normal
         if (!Estatus_ampliacion) {
             // Validaciones
-                //Verificar si se permiten prestamos multiples
-                //si no, verificar si no tiene ningun otro prestamo
-                //verificar cantidad maxima que puede pedir el socio (cantidad de dinero en acciones * Limite credito de acuerdos)
-                //Calcular el monto acumulado en prestamos vigentes (si esta cantidad rebasa su limite no puede proceder)
+            //Verificar si se permiten prestamos multiples
+            //si no, verificar si no tiene ningun otro prestamo
+            //verificar cantidad maxima que puede pedir el socio (cantidad de dinero en acciones * Limite credito de acuerdos)
+            //Calcular el monto acumulado en prestamos vigentes (si esta cantidad rebasa su limite no puede proceder)
 
             //Para hacer las validaciones anteriores necesitamos los datos del socio
             let query = "SELECT * FROM grupo_socio WHERE Socio_id = ? and Grupo_id = ?";
@@ -81,18 +81,18 @@ export const crear_prestamo = async (req: AdminRequest<PayloadCrearPrestamos>, r
             let Lista_socios_validacion = await prestamos_multiples(Grupo_id, [socio]);
             console.log(Lista_socios_validacion);
 
-            if(!Lista_socios_validacion[0].puede_pedir){
+            if (!Lista_socios_validacion[0].puede_pedir) {
                 return res.status(400).json({ code: 400, message: "El socio " + Lista_socios_validacion[0].Nombres + " " + Lista_socios_validacion[0].message });
             }
             //Verificar que la cantidad solicitada sea menor a su limite
-            if(Monto_prestamo > Lista_socios_validacion[0].Limite_credito_disponible! ){
+            if (Monto_prestamo > Lista_socios_validacion[0].Limite_credito_disponible!) {
                 return res.status(400).json({ code: 400, message: "La cantidad solicitada rebsasa su limite de credito" });
             }
             //Verificar si hay esa cantidad disponible en la caja
-                //Obtener la caja de la sesion activa
-                let caja = await obtener_caja_sesion(sesionActual.Sesion_id!);
-            if(caja < Monto_prestamo){
-                return res.status(400).json({ code: 400, message:"No hay suficiente cantidad en la caja" });
+            //Obtener la caja de la sesion activa
+            let caja = await obtener_caja_sesion(sesionActual.Sesion_id!);
+            if (caja < Monto_prestamo) {
+                return res.status(400).json({ code: 400, message: "No hay suficiente cantidad en la caja" });
             }
 
             // Crear Registro en prestamos
@@ -100,42 +100,43 @@ export const crear_prestamo = async (req: AdminRequest<PayloadCrearPrestamos>, r
             // return res.status(201).json({ code: 201, message: "Prestamo creado" });
         } else {
             // Verificar que el socio pueda generar un prestamo ampliado
-            if(!acuerdoActual.Ampliacion_prestamos){
-                return res.status(400).json({ code: 400, message:"No se permiten ampliar prestamos" })
+            if (!acuerdoActual.Ampliacion_prestamos) {
+                return res.status(400).json({ code: 400, message: "No se permiten ampliar prestamos" })
             }
 
             //Verificar que el prestamo no haya sido ampliado anteriormente
             let query_prestamo = "SELECT * FROM prestamos WHERE Prestamo_original_id = ? OR Estatus_ampliacion = 1"
             const [prestamo_original] = await db.query(query_prestamo, [Prestamo_original_id]) as [Prestamo[], any];
 
-            if(prestamo_original[0].Prestamo_original_id !== null){
-                return res.status(400).json({ code: 400, message:"Este prestamo ya fue ampliado una vez" });
+            if (prestamo_original[0].Prestamo_original_id !== null) {
+                return res.status(400).json({ code: 400, message: "Este prestamo ya fue ampliado una vez" });
             }
 
             //Asegurarse de que el monto sea igual o mayor a la cantidad faltante de pagar que el prestamo original
             let faltante = prestamo_original[0].Monto_prestamo - prestamo_original[0].Monto_pagado;
-            if(Monto_prestamo < faltante){
-                return res.status(400).json({ code: 400, message:"La cantidad no cubre el faltante del prestamo original"});
+            if (Monto_prestamo < faltante) {
+                return res.status(400).json({ code: 400, message: "La cantidad no cubre el faltante del prestamo original" });
             }
 
             let limite = limite_credito(Socio_id, Grupo_id, null, null, null);
-            if(limite[0] === 0){
-                return res.status(400).json({ code: 400, message:"La cantidad solicitada rebasa su limite de credito"});
+            if (limite[0] === 0) {
+                return res.status(400).json({ code: 400, message: "La cantidad solicitada rebasa su limite de credito" });
             }
-            
+
             // let dinero_extra = Monto_prestamo - faltante; //Preguntar que si no hay un espacio en la pantalla para ver lo que en realidad se da en dinero fisico
             //Pagar el prestamo original
             pagarPrestamo(Prestamo_original_id!, faltante, con);
             // Generar prestamo ampliado
             generar_prestamo(Grupo_id, campos_prestamo);
-            }
+        }
 
         await con.commit();
         // return res.status(201).json({ code: 201, message: "Ampliacion hecha" });
         return res.status(201).json({ code: 201, message: "Listo (:" });
     } catch (error) {
+        console.log('holiwi')
         await con.rollback();
-        console.log("Este es el error: "+error);
+        console.log("Este es el error: " + error);
         const { code, message } = getCommonError(error);
         return res.status(code).json({ code, message });
     } finally {
@@ -164,9 +165,9 @@ export const pagar_prestamos = async (req: AdminRequest<PayloadPagarPrestamos>, 
             const { Prestamo_id, Monto_abono } = Prestamos[pago_prestamo];
             await pagarPrestamo(Prestamo_id, Monto_abono, con);
         }
-        
+
         await con.commit();
-        
+
         res.status(200).json({ code: 200, message: 'Pagos realizados' });
     } catch (error) {
         console.log(error);
