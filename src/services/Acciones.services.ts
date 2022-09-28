9f8f967df126ea31ad033143c799f191ffe3850c
JaviZamudio <javier.elihu100@gmail.com>
2022-09-28 12:03:57 -0500

    Compra de acciones. Errores corregidos

--- src/services/Acciones.services.ts
+++ src/services/Acciones.services.ts
@@ -1,4 +1,8 @@
+import { PoolConnection } from "mysql2/promise";
+import db from "../config/database";
 import { obtener_acuerdos_activos } from "../utils/validaciones";
+import { obtenerAcuerdoActual } from "./Acuerdos.services";
+import { crear_transaccion } from "./Transacciones.services";
 
 /**
  * Funcion para comprar acciones para un socio en un grupo
@@ -8,13 +12,30 @@ import { obtener_acuerdos_activos } from "../utils/validaciones";
  * @returns Objeto de tipo OkPacket
  * @throws Error si no se puede comprar las acciones
  */
-export const comprar_acciones = async (Socio_id, Grupo_id, Cantidad) => {
-    // Verificar que la cantidad sea divisible por el costo de una accion
-    // Obtener el costo de una accion
-    const costo_accion = await obtener_costo_accion(Grupo_id);
-    if (Cantidad % costo_accion !== 0) {
-        throw `La cantidad de acciones no es divisible por el costo de una accion(${costo_accion})`;
+export const comprar_acciones = async (Socio_id, Grupo_id, Cantidad, con?: PoolConnection) => {
+    if(!con) {
+        con = await db.getConnection();
     }
+
+    // Agregar la accion a la relacion socio-grupo
+    let query = "UPDATE grupo_socio SET acciones = acciones + ? WHERE Socio_id = ? AND Grupo_id = ?";
+    await con.query(query, [Cantidad, Socio_id, Grupo_id]);
+
+    // Actualizar la cantidad de acciones del grupo en la sesion
+    query = `UPDATE sesiones
+    SET Acciones = Acciones + ?
+    WHERE Grupo_id = ?
+    ORDER BY Sesion_id DESC
+    LIMIT 1`;
+    await con.query(query, [Cantidad, Grupo_id]);
+
+    // Registrar la transaccion
+    await crear_transaccion({
+        Cantidad_movimiento: Cantidad,
+        Catalogo_id: "COMPRA_ACCION",
+        Socio_id,
+        Grupo_id,
+    }, con);
 }
 
 /**
@@ -24,6 +45,6 @@ export const comprar_acciones = async (Socio_id, Grupo_id, Cantidad) => {
  * @throws Error si no se puede obtener el costo de una accion
  */
 export async function obtener_costo_accion(Grupo_id: any) {
-    const acuerdos = await obtener_acuerdos_activos(Grupo_id);
+    const acuerdos = await obtenerAcuerdoActual(Grupo_id);
     return acuerdos.Costo_acciones;
 }
